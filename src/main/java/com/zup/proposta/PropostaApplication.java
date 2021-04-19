499af7bd2034bfd10eef6f114b7c22e38b7bee31
devblack21 <79933279+deviny99@users.noreply.github.com>
2021-04-19 16:20:05 -0300

    subindo estrutura do projeto

---
+++ src/main/java/com/zup/proposta/PropostaApplication.java
@@ -0,0 +1,17 @@
+package com.zup.proposta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableFeignClients
+@SpringBootApplication
+public class PropostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropostaApplication.class, args);
+	}
+
+}
