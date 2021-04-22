305b31dbf01c75599237a8005dc770661d75fd0c
devblack21 <79933279+deviny99@users.noreply.github.com>
2021-04-22 12:06:52 -0300

    helthcheck da aplicação

--- src/main/java/com/zup/academy/config/KeycloakAdapterConfig.java
+++ src/main/java/com/zup/academy/config/KeycloakAdapterConfig.java
@@ -1,90 +1,88 @@
-//package com.zup.academy.config;
-//import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-//import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-//import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter;
-//import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
-//import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
-//import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter;
-//import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Profile;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.core.session.SessionRegistryImpl;
-//import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
-//import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-//import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-//
-//@Profile({"prod"})
-//@KeycloakConfiguration
-//public class KeycloakAdapterConfig extends KeycloakWebSecurityConfigurerAdapter{
-//
-//
-//    /**
-//     * Registers the KeycloakAuthenticationProvider with the authentication manager.
-//     */
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(keycloakAuthenticationProvider());
-//    }
-//
-//    /**
-//     * Defines the session authentication strategy.
-//     */
-//    @Bean
-//    @Override
-//    protected NullAuthenticatedSessionStrategy sessionAuthenticationStrategy() {
-//        return new NullAuthenticatedSessionStrategy();
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception
-//    {
-//        super.configure(http);
-//        http.cors().and().csrf().disable()
-//                .authorizeRequests()
-//                .anyRequest().authenticated();
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
-//            KeycloakAuthenticationProcessingFilter filter) {
-//        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
-//        registrationBean.setEnabled(false);
-//        return registrationBean;
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
-//            KeycloakPreAuthActionsFilter filter) {
-//        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
-//        registrationBean.setEnabled(false);
-//        return registrationBean;
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean keycloakAuthenticatedActionsFilterBean(
-//            KeycloakAuthenticatedActionsFilter filter) {
-//        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
-//        registrationBean.setEnabled(false);
-//        return registrationBean;
-//    }
-//
-//    @Bean
-//    public FilterRegistrationBean keycloakSecurityContextRequestFilterBean(
-//            KeycloakSecurityContextRequestFilter filter) {
-//        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
-//        registrationBean.setEnabled(false);
-//        return registrationBean;
-//    }
-//
-//    @Bean
-//    @Override
-//    @ConditionalOnMissingBean(HttpSessionManager.class)
-//    protected HttpSessionManager httpSessionManager() {
-//        return new HttpSessionManager();
-//    }
-//}
+package com.zup.academy.config;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakSecurityContextRequestFilter;
+import org.keycloak.adapters.springsecurity.management.HttpSessionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+
+
+@KeycloakConfiguration
+public class KeycloakAdapterConfig extends KeycloakWebSecurityConfigurerAdapter{
+
+
+    /**
+     * Registers the KeycloakAuthenticationProvider with the authentication manager.
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    /**
+     * Defines the session authentication strategy.
+     */
+    @Bean
+    @Override
+    protected NullAuthenticatedSessionStrategy sessionAuthenticationStrategy() {
+        return new NullAuthenticatedSessionStrategy();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception
+    {
+        super.configure(http);
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**").hasRole("METRICS_ADMIN")
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+            KeycloakAuthenticationProcessingFilter filter) {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+            KeycloakPreAuthActionsFilter filter) {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean keycloakAuthenticatedActionsFilterBean(
+            KeycloakAuthenticatedActionsFilter filter) {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean keycloakSecurityContextRequestFilterBean(
+            KeycloakSecurityContextRequestFilter filter) {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+        registrationBean.setEnabled(false);
+        return registrationBean;
+    }
+
+    @Bean
+    @Override
+    @ConditionalOnMissingBean(HttpSessionManager.class)
+    protected HttpSessionManager httpSessionManager() {
+        return new HttpSessionManager();
+    }
+}
---
+++ src/test/java/com/zup/academy/global/healthcheck/HealthCheckTest.java
@@ -0,0 +1,49 @@
+package com.zup.academy.global.healthcheck;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Testa os endpoints de HelthCheck da aplicação")
+public class HealthCheckTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Verifica endpoint da saúde da aplicacao")
+    @WithMockUser(roles = "METRICS_ADMIN")
+    void verificaEndpointDaSaudeDaAplicacao() throws Exception {
+        MvcResult result = this.mockMvc.perform(get("/actuator/health")
+                .contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        Assertions.assertEquals(200,result.getResponse().getStatus());
+
+    }
+
+    @Test
+    @DisplayName("Não deve autorizar os endpoints das metricas para outros niveis de acesso")
+    @WithMockUser(roles = "USER")
+    void naoDeveAutorizarEndpointDeMetricas() throws Exception {
+        MvcResult result = this.mockMvc.perform(get("/actuator/health")
+                .contentType(MediaType.APPLICATION_JSON)).andReturn();
+
+        Assertions.assertEquals(403,result.getResponse().getStatus());
+
+    }
+
+
+
+
+}
